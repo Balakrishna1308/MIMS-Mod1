f84226d19287cb3967a6aef7f63e0838568f481f
Bala <balakrishna.malipeddi@gmail.com>
2023-10-07 22:13:56 +0530

    Commits saved.

---
+++ misc/TempCode_SearchJS.txt
@@ -0,0 +1,33 @@
+// Search.js
+import React, { useState } from 'react';
+
+
+
+import './Search.css'; // Import the CSS file for the Search component
+
+
+
+
+function Search(props) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    // Implement your search logic here
+    // You can use the 'searchTerm' state to perform the search
+    // Update the 'searchResults' state to display search results
+  };
+
+  return (
+    <div className="search">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+}
+
+export default Search;
---
+++ src/Notification.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './Notification.css';
+
+function Notification() {
+  const [showNotification, setShowNotification] = useState(false);
+
+  const handleShowNotification = () => {
+    setShowNotification(true);
+
+    // Automatically hide the notification after a few seconds (e.g., 3 seconds)
+    setTimeout(() => {
+      setShowNotification(false);
+    }, 3000); // 3000 milliseconds (3 seconds)
+  };
+
+  return (
+    <div className={`notification ${showNotification ? 'show' : ''} top-left`}>
+      <button className="close-button" onClick={() => setShowNotification(false)}>
+        &#x2716;
+      </button>
+      <p>You have one notification.</p>
+    </div>
+  );
+}
+
+export default Notification;
