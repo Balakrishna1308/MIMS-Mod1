d6e922bb2168a5a4cf8d5f9a7d18f41b2416834d
Bala <balakrishna.malipeddi@gmail.com>
2023-11-29 13:20:17 +0530

    commits saved.

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -74,3 +74,43 @@ function FileUpload() {
 }
 
 export default FileUpload;
+
+
+
+
+// function uploadFile(file) {
+//   const formData = new FormData();
+//   formData.append('file', file);
+
+//   return fetch('http://localhost:8080/api/files/upload', {
+//       method: 'POST',
+//       body: formData,
+//   })
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error('Error uploading file:', error));
+// }
+
+// function downloadFile(fileId) {
+//   fetch(`http://localhost:8080/api/files/download/${fileId}`)
+//       .then(response => {
+//           const disposition = response.headers.get('Content-Disposition');
+//           const filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+//           const matches = filenameRegex.exec(disposition);
+//           const filename = matches && matches.length > 1 ? matches[1] : 'download';
+
+//           response.blob().then(blob => {
+//               const url = window.URL.createObjectURL(new Blob([blob]));
+//               const link = document.createElement('a');
+//               link.href = url;
+//               link.setAttribute('download', filename);
+//               document.body.appendChild(link);
+//               link.click();
+//               document.body.removeChild(link);
+//           });
+//       })
+//       .catch(error => console.error('Error downloading file:', error));
+// }
+
+
+
