f9972cd87cae41383c48c25cae7a8e7798213932
Bala <balakrishna.malipeddi@gmail.com>
2023-11-29 17:28:14 +0530

    commits saved

--- src/Search.js
+++ src/Search.js
@@ -131,51 +131,85 @@
 
 
 
-import React, { useState } from 'react';
-import './Search.css';
+// import React, { useState } from 'react';
+// import './Search.css';
 
-function Search(props) {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+// function Search(props) {
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const [searchResults, setSearchResults] = useState([]);
 
-  const handleSearch = () => {
-    // Make an HTTP request to the search endpoint
-    fetch('http://localhost:8080/api/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ searchTerm }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        // Set the search results in the component state
-        setSearchResults(data);
-      })
-      .catch(error => {
-        console.error('Error searching items:', error);
-      });
-  };
+//   const handleSearch = () => {
+   
+//     fetch('http://localhost:8080/api/search', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ searchTerm }),
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+      
+//         setSearchResults(data);
+//       })
+//       .catch(error => {
+//         console.error('Error searching items:', error);
+//       });
+//   };
 
-  return (
-    <div className="search-container">
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+//   return (
+//     <div className="search-container">
+//       <input
+//         type="text"
+//         placeholder="Search..."
+//         value={searchTerm}
+//         onChange={(e) => setSearchTerm(e.target.value)}
+//       />
 
-      <button onClick={handleSearch}>Search</button>
+//       <button onClick={handleSearch}>Search</button>
 
-      {/* Display the search results */}
-      <div className="search-results">
-        {searchResults.map(item => (
-          <div key={item.id}>{item.title}</div>
-        ))}
-      </div>
-    </div>
-  );
+      // {
+        /* Display the search results */
+      // }
+//       <div className="search-results">
+//         {searchResults.map(item => (
+//           <div key={item.id}>{item.title}</div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Search;
+
+
+
+
+
+
+import React, { useState, useEffect } from 'react';
+
+function FeedbackComponent() {
+    const [feedbackList, setFeedbackList] = useState([]);
+
+    useEffect(() => {
+        // Fetch feedback data from your Spring Boot API
+        fetch('http://localhost:8080/api/feedback/getAll')
+            .then(response => response.json())
+            .then(data => setFeedbackList(data))
+            .catch(error => console.error('Error fetching feedback:', error));
+    }, []); // Empty dependency array ensures the effect runs once when the component mounts
+
+    return (
+        <div>
+            <h2>Feedback List</h2>
+            <ul>
+                {feedbackList.map(feedback => (
+                    <li key={feedback.id}>{feedback.content}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default Search;
+export default FeedbackComponent;
