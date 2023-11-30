8f9e56d894aa61d781a5f0a563bf17f9522a44a8
Bala <balakrishna.malipeddi@gmail.com>
2023-11-30 12:42:58 +0530

    commits saved.

--- src/Search.js
+++ src/Search.js
@@ -131,86 +131,86 @@
 
 
 // Search.js
-// import React, { useState } from 'react';
-// import './Search.css';
-
-// function Search(props) {
-//   const [searchTerm, setSearchTerm] = useState('');
-//   const [searchResults, setSearchResults] = useState([]);
-
-//   const handleSearch = () => {
-   
-//     fetch('http://localhost:8080/api/search', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ searchTerm }),
-//     })
-//       .then(response => response.json())
-//       .then(data => {
-      
-//         setSearchResults(data);
-//       })
-//       .catch(error => {
-//         console.error('Error searching items:', error);
-//       });
-//   };
-
-//   return (
-//     <div className="search-container">
-//       <input
-//         type="text"
-//         placeholder="Search..."
-//         value={searchTerm}
-//         onChange={(e) => setSearchTerm(e.target.value)}
-//       />
-
-//       <button onClick={handleSearch}>Search</button>
-
-      // {
-        /* Display the search results */
-      // }
-//       <div className="search-results">
-//         {searchResults.map(item => (
-//           <div key={item.id}>{item.title}</div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Search;
-
-
-
-
-
-
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 
-function FeedbackComponent() {
-    const [feedbackList, setFeedbackList] = useState([]);
+function Search(props) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
-    useEffect(() => {
-        
-        fetch('http://localhost:8080/api/feedback/getAll')
-            .then(response => response.json())
-            .then(data => setFeedbackList(data))
-            .catch(error => console.error('Error fetching feedback:', error));
-    }, []); 
-    return (
-        <div>   
-            <h2>Feedback List</h2>
-            <ul>
-                {feedbackList.map(feedback => (
-                    <li key={feedback.id}>{feedback.content}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  const handleSearch = () => {
+   
+    fetch('http://localhost:8080/api/search', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ searchTerm }),
+    })
+      .then(response => response.json())
+      .then(data => {
+      
+        setSearchResults(data);
+      })
+      .catch(error => {
+        console.error('Error searching items:', error);
+      });
+  };
+
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      <button onClick={handleSearch}>Search</button>
+
+       {
+        /* Display the search results */
+       }
+      <div className="search-results">
+       {searchResults.map(item => (
+          <div key={item.id}>{item.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default FeedbackComponent;
+export default Search;
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import './Search.css';
+
+// function FeedbackComponent() {
+//     const [feedbackList, setFeedbackList] = useState([]);
+
+//     useEffect(() => {
+        
+//         fetch('http://localhost:8080/api/feedback/getAll')
+//             .then(response => response.json())
+//             .then(data => setFeedbackList(data))
+//             .catch(error => console.error('Error fetching feedback:', error));
+//     }, []); 
+//     return (
+//         <div>   
+//             <h2>Feedback List</h2>
+//             <ul>
+//                 {feedbackList.map(feedback => (
+//                     <li key={feedback.id}>{feedback.content}</li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
+
+// export default FeedbackComponent;
 
