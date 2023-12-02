1ece45b8a8fb9174dee44eb3d1be050186e74e24
Bala <balakrishna.malipeddi@gmail.com>
2023-12-02 12:19:16 +0530

    commits saved.

--- src/main/java/com/myshow4all/student_internship_program/controller/AuthController.java
+++ src/main/java/com/myshow4all/student_internship_program/controller/AuthController.java
@@ -6,14 +6,12 @@ import com.myshow4all.student_internship_program.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // AuthController.java
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
     @Autowired
     private UserService userService;
