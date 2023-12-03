2cc5e99e050fd6cad4dd1f05c2f5472ff85645d6
Bala <balakrishna.malipeddi@gmail.com>
2023-12-03 21:06:13 +0530

    commits saved.

--- src/App.js
+++ src/App.js
@@ -1,83 +1,136 @@
 
 
 
-import React from 'react';
-import './App.css'; 
-// import Calendar from './Calendar'; 
+// import React from 'react';
+// import './App.css'; 
+// import Search from './Search';
+// import Notification from './Notification';
+// import Tasks from './Tasks';
+// import FileUpload from './FileUpload';
+
+// import Progress from './Progress';
+
+// import Feedback from './Feedback';
+// import Certificate from './Certificate';
+
+
+
+// import Calendara from './Calendara';
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="container">
+     
+      
+      
+
+      
+      
+//       <div className="flashcard">
+//         <div className="flashcard-content">
+//           <h3>Calendar</h3>
+//           <p>It is where you can schedule your tasks, appointments, and events efficiently, making it a crucial part of any productivity application.</p>
+//         </div>
+//       </div>
+
+
+
+//       {
+      
+      /* <Calendar /> */
+      
+      // }
+     
+
+    // <FileUpload />
+
+
+    // {
+    
+    /* <FileUploadFlashcard /> */
+    
+    // }
+
+
+//     <Search />
+//     <Progress status="In Progress" percentage={75} />
+    
+//     <Tasks/>
+//     <Notification />
+    
+//     <Certificate />
+
+//     <Feedback/>
+
+
+
+
+//     <Calendara />
+
+
+
+    
+
+
+    
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+// App.js
+import React, { useState } from 'react';
+import './App.css';
+import Login from './Login';
+// import Dashboard from './Dashboard';
 import Search from './Search';
 import Notification from './Notification';
 import Tasks from './Tasks';
 import FileUpload from './FileUpload';
-
-
-
-
-// import './Flashcard.css';
-
-
-
-
 import Progress from './Progress';
-// import FileUploadFlashcard from './FileUploadFlashcard';
 import Feedback from './Feedback';
 import Certificate from './Certificate';
-
-
-
 import Calendara from './Calendara';
 
-
-
-
-
-
-
-
-
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  // Function to handle authentication status
+  const handleAuthentication = (status) => {
+    setAuthenticated(status);
+  };
+
   return (
     <div className="container">
-     
-      
-      
-
-      
-      
-      <div className="flashcard">
-        <div className="flashcard-content">
-          <h3>Calendar</h3>
-          <p>It is where you can schedule your tasks, appointments, and events efficiently, making it a crucial part of any productivity application.</p>
-        </div>
-      </div>
-
-
-
-      {/* <Calendar /> */}
-     
-
-    <FileUpload />
-    {/* <FileUploadFlashcard /> */}
-    <Search />
-    <Progress status="In Progress" percentage={75} />
-    
-    <Tasks/>
-    <Notification />
-    
-    <Certificate />
-
-    <Feedback/>
-
-
-
-
-    <Calendara />
-
-
-
-    
-
-
-    
+      {authenticated ? (
+        // Render Dashboard if authenticated
+        <>
+          <Search />
+          <Progress status="In Progress" percentage={75} />
+          <Tasks />
+          <Notification />
+          <Certificate />
+          <Feedback />
+          <Calendara />
+        </>
+      ) : (
+        // Render Login if not authenticated
+        <Login setAuthenticated={handleAuthentication} />
+      )}
     </div>
   );
 }
