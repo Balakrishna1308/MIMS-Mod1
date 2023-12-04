fcabfb3b49a043a9aaf902335acfda39acb249e9
Bala <balakrishna.malipeddi@gmail.com>
2023-12-04 12:02:34 +0530

    commits saved.

--- src/main/java/com/myshow4all/student_internship_program/controller/LoginController.java
+++ src/main/java/com/myshow4all/student_internship_program/controller/LoginController.java
@@ -1,31 +1,31 @@
-package com.myshow4all.student_internship_program.controller;
-
-// LoginController.java
-
-
-import com.myshow4all.student_internship_program.dto.LoginRequest;
-import com.myshow4all.student_internship_program.entity.User;
-import com.myshow4all.student_internship_program.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
-public class LoginController {
-
-    @Autowired
-    private UserService userService;
-
-    @PostMapping("/login")
-    public ResponseEntity<User> loginUser(@RequestBody LoginRequest loginRequest) {
-        User user = userService.loginUser(loginRequest.getUsername(), loginRequest.getPassword());
-
-        if (user != null) {
-            return ResponseEntity.ok(user);
-        } else {
-            return ResponseEntity.badRequest().build();
-        }
-    }
-}
+//package com.myshow4all.student_internship_program.controller;
+//
+//// LoginController.java
+//
+//
+//import com.myshow4all.student_internship_program.dto.LoginRequest;
+//import com.myshow4all.student_internship_program.entity.User;
+//import com.myshow4all.student_internship_program.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api")
+//@CrossOrigin(origins = "http://localhost:3000")
+//public class LoginController {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<User> loginUser(@RequestBody LoginRequest loginRequest) {
+//        User user = userService.loginUser(loginRequest.getUsername(), loginRequest.getPassword());
+//
+//        if (user != null) {
+//            return ResponseEntity.ok(user);
+//        } else {
+//            return ResponseEntity.badRequest().build();
+//        }
+//    }
+//}
